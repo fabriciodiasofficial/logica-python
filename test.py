772fc79a963000acbd9b81c679352ae5cbd473b0
fabriciodiasofficial <142854546+fabriciodiasofficial@users.noreply.github.com>
2024-03-30 17:23:00 -0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,51 @@
+print "Hello World!"
+-------------------
+a = 10
+print (a)
+-------------------
+b = 1.2
+print (b)
+------------------
+c = "Olá Mundo"
+print (c)
+------------------
+nome = input("Entre com seu nome: ")
+print(nome)
+-------------------
+num = int(input("Entre com um numero? : "))
+print(num)
+-------------------
+altura = float(input("Entre com a sua altura :"))
+print (altura)
+--------------------
+print ("Apostila"+"Python")  pg9
+----------------------
+a = 'Programação'
+b = 'Python'
+c = a + b
+print(c)
+----------------------
+# Retorna o tamanho da string
+teste = "Apostila de Python"
+print (len(teste))
+---------------------
+#Retorna a string com a primeira letra maiúscula
+a = "python"
+print (a.capitalize())
+----------------------
+#Informa quantas vezes um caractere (ou uma sequência de caracteres) aparece na string
+b = "Linguagem Python"
+print (b.count("n"))
+-----------------------
+
+
+
+
+
+
+
+
+
+
+
+
